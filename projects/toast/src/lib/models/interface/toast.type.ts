ee67fd3c56951d7b22bed6913b5ba8b013a390c8
charlieTai <charlie.tai@tpisoftware.com>
2025-01-04 23:38:16 +0800

    modify : 調整型別

--- projects/toast/src/lib/models/interface/toast.type.ts
+++ projects/toast/src/lib/models/interface/toast.type.ts
@@ -1,9 +1,14 @@
 import { toastType } from '../enums/type.enum';
 
+// Toast 創建時的輸入類型
 export type Toast = {
-  id: string;
   message: string;
   type: toastType;
   duration?: number;
   icon?: string;
+};
+
+// 完整的 Toast 類型，包括自動生成的 ID
+export type ToastWithId = Toast & {
+  id: string;
 };
